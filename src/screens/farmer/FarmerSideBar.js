cab1d193025f87bc9e00c37ba5a6d61e3b1941a2
Obi NkemjIka Derick <obinkemjikaderick@Administrators-MacBook-Pro-7.local>
2021-05-25 22:44:35 +0100

    Customizing the Farmer Dashboard, Adding Icons and Adding Font Awesome

--- src/screens/farmer/FarmerSideBar.js
+++ src/screens/farmer/FarmerSideBar.js
@@ -6,98 +6,48 @@ const FarmerSidebar = () => {
             <section className="sidebar" id="sidebar-scroll">
                 <ul className="sidebar-menu">
                     <li className="active treeview">
-                        <a className="waves-effect waves-dark" href="index.html">
+                        <a className="waves-effect waves-dark" href="/farmer">
                             <i className="icon-speedometer"></i><span> Dashboard</span>
                         </a>
                     </li>
-                    <li className="treeview"><a className="waves-effect waves-dark" href="#!"><i
-                        className="icon-briefcase"></i><span> Equipment Requests</span><i className="icon-arrow-down"></i></a>
-                        <ul className="treeview-menu">
-                            <li><a className="waves-effect waves-dark" href="accordion.html"><i
-                                className="icon-arrow-right"></i> Accordion</a></li>
-                            <li><a className="waves-effect waves-dark" href="button.html"><i
-                                className="icon-arrow-right"></i> Button</a></li>
-                            <li><a className="waves-effect waves-dark" href="label-badge.html"><i
-                                className="icon-arrow-right"></i> Label Badge</a></li>
-                            <li><a className="waves-effect waves-dark" href="bootstrap-ui.html"><i
-                                className="icon-arrow-right"></i> Grid system</a></li>
-                            <li><a className="waves-effect waves-dark" href="box-shadow.html"><i
-                                className="icon-arrow-right"></i> Box Shadow</a></li>
-                            <li><a className="waves-effect waves-dark" href="color.html"><i
-                                className="icon-arrow-right"></i> Color</a></li>
-                            <li><a className="waves-effect waves-dark" href="light-box.html"><i
-                                className="icon-arrow-right"></i> Light Box</a></li>
-                        </ul>
+                    <li className="treeview">
+                        <a className="waves-effect waves-dark" href="#!"><i className="icon-briefcase"></i><span> Equipment Requests</span></a>  
+                    </li>
+                    <li>
+                        <a className="waves-effect waves-dark" href="/farmer/new_request"> New Request</a>
+                    </li>
+                    <li>
+                        <a className="waves-effect waves-dark" href="/farmer/view_requests">View Requests</a>
                     </li>
 
-                    <li className="treeview"><a className="waves-effect waves-dark" href="#!"><i
-                        className="icon-chart"></i><span>Equipment Report</span><i className="icon-arrow-down"></i></a>
-                        <ul className="treeview-menu">
-                            <li><a className="waves-effect waves-dark" href="float-chart.html"><i
-                                className="icon-arrow-right"></i> Float Charts</a></li>
-                            <li><a className="waves-effect waves-dark" href="morris-chart.html"><i
-                                className="icon-arrow-right"></i> Morris Charts</a></li>
-                        </ul>
+                    <li className="treeview">
+                        <a className="waves-effect waves-dark" href="#!"><i className="icon-chart"></i><span>Equipment Report</span></a>   
+                    </li>
+                    <li>
+                        <a className="waves-effect waves-dark" href="float-chart.html"> Rate Job</a>
+                    </li>
+                    <li>
+                        <a className="waves-effect waves-dark" href="morris-chart.html">Complaints</a>
                     </li>
 
-                    <li className="treeview"><a className="waves-effect waves-dark" href="#!"><i
-                        className="icon-book-open"></i><span> Farm</span><i className="icon-arrow-down"></i></a>
-                        <ul className="treeview-menu">
-                            <li><a className="waves-effect waves-dark" href="form-elements-bootstrap.html"><i
-                                className="icon-arrow-right"></i> Form Elements Bootstrap</a></li>
-
-                            <li><a className="waves-effect waves-dark" href="form-elements-advance.html"><i
-                                className="icon-arrow-right"></i> Form Elements Advance</a></li>
-                        </ul>
+                    <li className="treeview">
+                        <a className="waves-effect waves-dark" href="#!"><i className="icon-book-open"></i><span> Farm</span></a>
+                    </li>
+                    <li>
+                        <a className="waves-effect waves-dark" href="form-elements-bootstrap.html">Add Farm</a>
+                    </li>
+                    <li>
+                        <a className="waves-effect waves-dark" href="form-elements-advance.html"> Manage Farm</a>
                     </li>
 
-                    <li className="treeview"><a className="waves-effect waves-dark" href="#!"><i
-                        className="icofont icofont-company"></i><span>Profile</span><i
-                        className="icon-arrow-down"></i></a>
-                        <ul className="treeview-menu">
-                            <li>
-                                <a className="waves-effect waves-dark" href="#!">
-                                    <i className="icon-arrow-right"></i>
-                                    Level Two
-                                </a>
-                            </li>
-                            <li className="treeview">
-                                <a className="waves-effect waves-dark" href="#!">
-                                    <i className="icon-arrow-right"></i>
-                                    <span>Level Two</span>
-                                    <i className="icon-arrow-down"></i>
-                                </a>
-                                <ul className="treeview-menu">
-                                    <li>
-                                        <a className="waves-effect waves-dark" href="#!">
-                                            <i className="icon-arrow-right"></i>
-                                            Level Three
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a className="waves-effect waves-dark" href="#!">
-                                            <i className="icon-arrow-right"></i>
-                                            <span>Level Three</span>
-                                            <i className="icon-arrow-down"></i>
-                                        </a>
-                                        <ul className="treeview-menu">
-                                            <li>
-                                                <a className="waves-effect waves-dark" href="#!">
-                                                    <i className="icon-arrow-right"></i>
-                                                    Level Four
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a className="waves-effect waves-dark" href="#!">
-                                                    <i className="icon-arrow-right"></i>
-                                                    Level Four
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
+                    <li className="treeview">
+                        <a className="waves-effect waves-dark" href="#!"><i className="icon-user"></i><span>Profile</span></a>
+                    </li>
+                    <li>
+                        <a className="waves-effect waves-dark" href="#!"><i class="fas fa-unlock-alt"></i>Change Password</a>
+                    </li>
+                    <li className="treeview">
+                        <a className="waves-effect waves-dark" href="#!"><i class="fas fa-key"></i>Edit Details</a>
                     </li>
                 </ul>
             </section>
