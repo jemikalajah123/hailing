dbd5779bc1fc99764dd5892c73f05eefa43a1cc4
Obi NkemjIka Derick <obinkemjikaderick@Administrators-MacBook-Pro-7.local>
2021-05-25 16:06:56 +0100

    Minor Fix to App State Management

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import FarmerState from './context/farmer/FarmerState'
-import AdminState from './context/Admin/FarmerState'
-import InvestorState from './context/Investor/FarmerState'
-import OperatorState from './context/Operator/FarmerState'
+import AdminState from './context/admin/AdminState'
+import InvestorState from './context/investor/InvestorState'
+import OperatorState from './context/operator/OperatorState'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Sidebar from './components/Sidebar';
