0e70601894c527e6550fcf383bcbca32c8bd7853
Obi NkemjIka Derick <obinkemjikaderick@Administrators-MacBook-Pro-7.local>
2021-05-25 15:57:02 +0100

    Set up Admin State context

---
+++ src/context/operator/farmerReducer.js
@@ -0,0 +1,18 @@
+import{
+    SET_LOADING,
+} from '../types'
+
+const farmerReducer = (state, action) => {
+    switch(action.type){
+        
+        case SET_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        default:
+            return state;
+    }
+}
+
+export default farmerReducer
